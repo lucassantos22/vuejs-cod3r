e311ff00191bd3f7d5f0adc0c38101295f4b2031
Lucas Santos <lucas.afsantos@hotmail.com>
2019-02-27 00:29:09 -0300

    Mais uma aula feita

---
+++ condicionais-e-listas/aula-03-listas-v1/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el:"#app",
+    data:{
+        cores:["vermelho", "verde", "amarelo", "azul"],
+        pessoas:[
+            {
+                nome:"Ana",
+                idade:26,
+                peso:59
+            }, 
+            {
+                nome:"Guilherme",
+                idade:22,
+                peso:76
+            }
+        ]
+    }
+})
