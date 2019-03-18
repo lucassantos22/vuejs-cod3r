b3e64ec8c75758b18b093ee75acc71b5ae431ad5
Lucas Santos <lucas.afsantos@hotmail.com>
2019-03-17 23:14:34 -0300

    Mais uma aula pronta

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,27 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		pessoa: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		condicao: false
+	},
+	methods: {
+		alterarCondicao() {
+			this.condicao = !this.condicao;
+		}
+	}
+});
