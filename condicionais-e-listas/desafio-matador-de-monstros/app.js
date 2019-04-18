4affdb0d25bfaeb77766c2a471f298abb7ac2d1b
Lucas Santos <lucas.afsantos@hotmail.com>
2019-04-18 16:25:50 -0300

    Commit do matador de monstros

--- condicionais-e-listas/desafio-matador-de-monstros/app.js
+++ condicionais-e-listas/desafio-matador-de-monstros/app.js
@@ -2,22 +2,51 @@ new Vue({
     el: "#app",
     data:{
         gameStatus: false,
-        playerLife: 0,
-        monsterLife: 100
+        playerLife: 100,
+        monsterLife: 100,
+        logs:[]
     },
     methods:{
         iniciarJogo(){
             this.gameStatus = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+        },
+        atacar(special=false){
+            if(!special){
+                this.playerLife = Math.max((this.playerLife - (Math.random() * 20)).toFixed(), 0);
+                this.monsterLife = Math.max((this.monsterLife - (Math.random() * 15)).toFixed(), 0);
+            }else{
+                this.playerLife = Math.max((this.playerLife - (Math.random() * 15)).toFixed(), 0)
+                this.monsterLife = Math.max((this.monsterLife - (Math.random() * 20)).toFixed(), 0)
+            }
+
+        },
+        curar(){
+            const valorCuraHeroi = (Math.random() * 10);
+            const valorDano = Math.random() * 5;
+            const valorCuraMonstro = Math.random() * 3;
+            this.playerLife = Math.round(this.playerLife + valorCuraHeroi - valorDano);
+            this.monsterLife = Math.round(this.monsterLife + valorCuraMonstro);
+        },
+        registerLog(text){
+            this.logs.unshift(text);
         }
     },
     computed:{
         hasResult(){
-            return this.playerLife == 0 || this.playerLife == 0;
+            return this.playerLife <= 0 || this.monsterLife <= 0;
         }
     },
     watch:{
-
+        hasResult(result){
+            if(result) this.gameStatus = false;
+        },
+        playerLife(life, lifeBeforeDamage){
+            this.registerLog("O player perdeu " + (lifeBeforeDamage - life) +" de vida");
+        },
+        monsterLife(life, lifeBeforeDamage){
+            this.registerLog("O monstro perdeu " + (lifeBeforeDamage - life) +" de vida");  
+        }
     }
 })
