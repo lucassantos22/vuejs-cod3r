7fde04a833da5137b250409080b8cbb7149249ac
Lucas Santos <lucas.afsantos@hotmail.com>
2019-06-02 02:21:32 -0300

    Aulas feitas

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot name="fonte"></slot>
+        <slot name="texto"></slot>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+        margin: 20px
+    }
+
+    /* h1{
+        color: red 
+    } Apesar do "h1" não ser visível nesse arquivo, o "h1" é passado do Citacoes para cá por slot, então ele existe nesse componente*/ 
+
+</style>
