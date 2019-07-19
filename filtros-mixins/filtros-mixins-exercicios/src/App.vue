3de233e2ebaeebcb2f0956fe6005a60db43f4df7
Lucas Santos <lucas.afsantos@hotmail.com>
2019-07-19 01:33:04 -0300

    Filtros estudados

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{cpf | filtroCpf}}</p>
+		<input type="text" :value="cpf | filtroCpf">
+	</div>
+</template>
+
+<script>
+export default {
+	filters:{
+		filtroCpf(valor){
+			let arr = valor.split('');
+			arr.splice(3,0,".");
+			arr.splice(7,0,".");
+			arr.splice(11,0,"-");
+			return arr.join('');
+		}
+	},
+	data(){
+		return{
+			cpf:"11817799002"
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
