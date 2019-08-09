1840b283f3893f43adbbd1f07b136ad67c183a33
Lucas Santos <lucas.afsantos@hotmail.com>
2019-08-09 01:09:16 -0300

    Estudando routes

---
+++ rotas/rotas-exercicios/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from "vue-router"
+import Inicio from "./componentes/Inicio"
+import Usuario from "./componentes/usuario/Usuario"
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history', // Ou hash
+    routes: [{
+        path: '/',
+        component: Inicio
+    },
+    {
+        path: '/usuario',
+        component: Usuario
+    }]
+})
