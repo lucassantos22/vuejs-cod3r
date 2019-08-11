ae78a8801d7875c66469b44090c682e86910959e
Lucas Santos <lucas.afsantos@hotmail.com>
2019-08-11 20:47:27 -0300

    Estudando mais sobre Router

---
+++ rotas/rotas-exercicios/src/componentes/template/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <li><router-link to="/" active-class="active" exact>Início</router-link></li>
+            <li><router-link to="/usuario" active-class="active">Usuário</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- rotas/rotas-exercicios/src/componentes/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/componentes/usuario/Usuario.vue
@@ -1,12 +1,18 @@
 <template>
     <div id="usuario">
         <h2>Usuarios</h2>
+        <hr/>
+        <button sucesso @click="voltar"> Voltar </button>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        voltar(){
+            this.$router.push('/');
+        }
+    },
 }
 </script>
 
